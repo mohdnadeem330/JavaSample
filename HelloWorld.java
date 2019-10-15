a24c7aa1c01f50e7c2bcf64413d09a5208542a7a
Mohammad Nadeem <mohd_nadeem60@yahoo.com>
2019-10-15 20:47:04 +0530

    Jenkins fire

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,13 +2,15 @@ class HelloWorld {
 
 	HelloWorld()
 	{
-		
-	}
+
+
 
 public static void main(String args[]) {
 
 System.out.println("Hello Jenkins Hi from GitHub Without Build"); 
 System.out.println("Finally I have done it!!!!!!!");
+System.out.println("Jenkins fire from the macOS");
 
+}
 }
 }
