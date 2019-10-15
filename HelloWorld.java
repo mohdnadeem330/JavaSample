315e300ac67c09b62498dccb11fa6628b07d87ca
Mohammad Nadeem <mohd_nadeem60@yahoo.com>
2019-10-15 20:49:39 +0530

    Jenkins of macOS

--- HelloWorld.java
+++ HelloWorld.java
@@ -9,7 +9,7 @@ public static void main(String args[]) {
 
 System.out.println("Hello Jenkins Hi from GitHub Without Build"); 
 System.out.println("Finally I have done it!!!!!!!");
-System.out.println("Jenkins fire from the macOS");
+System.out.println("Jenkins fire from the macOS Yay!!!");
 
 }
 }
