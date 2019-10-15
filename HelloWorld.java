b43560c66d7093184d41a8403bc2bc10188aab24
Mohammad Nadeem <mohd_nadeem60@yahoo.com>
2019-10-15 17:35:04 +0530

    Hello Jenkins

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ class HelloWorld {
 
 public static void main(String args[]) {
 
-System.out.println("Hello Jenkins Hi from hub"); 
+System.out.println("Hello Jenkins Hi from GitHub Without Build"); 
 }
 }
