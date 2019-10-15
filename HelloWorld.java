348e29e6b7c2c1d12d754cb501709a955fba5ab3
Mohammad Nadeem <mohd_nadeem60@yahoo.com>
2019-10-15 20:42:07 +0530

    Java from mac

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,14 @@
 class HelloWorld {
 
+	HelloWorld()
+	{
+		
+	}
+
 public static void main(String args[]) {
 
 System.out.println("Hello Jenkins Hi from GitHub Without Build"); 
 System.out.println("Finally I have done it!!!!!!!");
+
 }
 }
