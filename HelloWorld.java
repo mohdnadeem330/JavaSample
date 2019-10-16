0b528b10015334e8f3945d6b055495e87cc4e0cd
Mohammad Nadeem <mohd_nadeem60@yahoo.com>
2019-10-16 12:43:49 +0530

    Commit from windows

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,8 @@
 class HelloWorld {
+    {
+        HelloWorld();
+
+    }
 
 	
 
@@ -8,6 +12,7 @@ public static void main(String args[]) {
 System.out.println("Hello Jenkins Hi from GitHub Without Build"); 
 System.out.println("Finally I have done it!!!!!!!");
 System.out.println("Jenkins fire from the macOS Yay!!!");
+System.out.println("This is the commit from Windows");
 
 }
 }
